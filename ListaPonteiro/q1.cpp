cf5b47e566342330c8bfd9a62d2d1776c95cfa63
Arthur Neves <>
2023-01-30 11:55:27 -0300

    questões

--- ListaPonteiro/q1.cpp
+++ ListaPonteiro/q1.cpp
@@ -1,21 +1,12 @@
 #include<iostream>
 
-int *criarVetor(int tam){
-    int lista[tam];
-    int v;
-    for(int i=0;i<tam;i++){
-        std::cout<<"Digite um valor para o elemento do vetor: "<<i+1<<std::endl;
-        std::cin>>v;
-        lista[i]=v;
-    }
-    return lista;
-}
 int main(){
     int tamanho;
-    std::cout<<"Digite o tamanho do vetor: ";
-    std::cin>>tamanho;
-    int *endereco;
-    endereco=criarVetor(tamanho);
-    std::cout<<*endereco;
+    std::string nome;
+    std::getline(std::cin,nome);
+    std::cout<<nome<<std::endl;
+
     return 0;
-}
+};
+
+
